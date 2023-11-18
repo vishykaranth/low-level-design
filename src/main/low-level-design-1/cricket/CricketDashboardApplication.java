539be8a807c94af23472ab49512b9a7d49e92485
vishykaranth <33781899+vishykaranth@users.noreply.github.com>
2023-11-18 22:19:42 +0530

    Refactorings.

---
+++ src/main/low-level-design-1/cricket/CricketDashboardApplication.java
@@ -0,0 +1,15 @@
+package cricket;
+
+import cricket.models.Match;
+import cricket.models.Team;
+
+public class CricketDashboardApplication {
+    public static void main(String[] args) {
+        Team india = new Team("India");
+        Team austriala = new Team("Austriala");
+        Match match1 = new Match("Test Match", india, austriala, 5, 2);
+        match1.addPlayersToTeam(india, new String[]{"P1", "P2", "P3", "P4", "P5"});
+        match1.addPlayersToTeam(austriala, new String[]{"P1", "P2", "P3", "P4", "P5"});
+        match1.startMatch(india);
+    }
+}

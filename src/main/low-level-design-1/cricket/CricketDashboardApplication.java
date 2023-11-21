1809877952f624fc2c116aa237d0344becea3b55
vishykaranth <33781899+vishykaranth@users.noreply.github.com>
2023-11-21 18:19:22 +0530

    Refactored.

--- src/main/low-level-design-1/cricket/CricketDashboardApplication.java
+++ src/main/low-level-design-1/cricket/CricketDashboardApplication.java
@@ -6,7 +6,7 @@ import cricket.models.Team;
 public class CricketDashboardApplication {
     public static void main(String[] args) {
         Team india = new Team("India");
-        Team austriala = new Team("Austriala");
+        Team austriala = new Team("Australia");
         Match match1 = new Match("Test Match", india, austriala, 5, 2);
         match1.addPlayersToTeam(india, new String[]{"P1", "P2", "P3", "P4", "P5"});
         match1.addPlayersToTeam(austriala, new String[]{"P1", "P2", "P3", "P4", "P5"});
--- src/main/low-level-design-1/ledger/Driver.java
+++ src/main/low-level-design-1/ledger/Driver.java
@@ -11,12 +11,10 @@ import java.util.stream.Stream;
 public class Driver {
     public static void main(String[] args) {
         CommandManager commandManager = new CommandManager();
-        String filePath = "E:\\jobhunt\\codebase\\github\\low-level-design\\src\\main\\java\\ledger\\files\\input01.txt";
+        String filePath = "E:\\jobhunt\\codebase\\github\\low-level-design\\src\\main\\low-level-design-1\\ledger\\files\\input01.txt";
         Path path = Paths.get(filePath);
         try (Stream<String> stream = Files.lines(path)) {
-            stream.forEach(
-                    fileLine -> commandManager.executeCommand(fileLine)
-            );
+            stream.forEach(commandManager::executeCommand);
         } catch (IOException e) {
             e.printStackTrace();
         }
